496f48cacd03c2e15bc76d1e7cf10708510160ec
dnarol9 <lorand.szabo9@gmail.com>
2022-01-20 13:05:06 +0100

    .split() added

--- python/calculator.py
+++ python/calculator.py
@@ -1,20 +1,18 @@
-print("ENTeR CALCULATION IN THIS FORMAT: +23, *23, /23")
-data = list(input())
+print("ENTER CALCULATION IN THIS FORMAT: + 2 3, * 2 3, / 2 3")
+data = input().split()
 print(data)
 n1 = data[1]
 n2 = data[2]
 numbers = []
 numbers.append(n1)
 numbers.append(n2)
+numbers = [int(x)for x in numbers]
 if "*" in data:
    print(n1,"*" ,n2)
-   numbers = [int(x)for x in numbers]
    print(numbers[0] * numbers[1])
 elif "+" in data:
     print(n1,"+", n2)
-    numbers = [int(x)for x in numbers]
     print(numbers[0] + numbers[1])
 elif "/" in data:
     print(n1,"/", n2)
-    numbers = [int(x)for x in numbers]
     print(numbers[0] / numbers[1])
