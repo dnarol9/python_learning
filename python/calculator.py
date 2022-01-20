1d384932f696f7a7aaee0824eac444d6eab6329b
dnarol9 <lorand.szabo9@gmail.com>
2022-01-20 12:52:37 +0100

    cean up

--- python/calculator.py
+++ python/calculator.py
@@ -1,32 +1,20 @@
 print("ENTeR CALCULATION IN THIS FORMAT: +23, *23, /23")
 data = list(input())
+print(data)
+n1 = data[1]
+n2 = data[2]
+numbers = []
+numbers.append(n1)
+numbers.append(n2)
 if "*" in data:
-   print(data)
-   n1 = data[1]
-   n2 = data[2]
-   numbers = []
-   numbers.append(n1)
-   numbers.append(n2)
    print(n1, n2)
    numbers = [int(x)for x in numbers]
    print(numbers[0] * numbers[1])
 elif "+" in data:
-    print(data)
-    n1 = data[1]
-    n2 = data[2]
-    numbers = []
-    numbers.append(n1)
-    numbers.append(n2)
     print(n1, n2)
     numbers = [int(x)for x in numbers]
     print(numbers[0] + numbers[1])
 elif "/" in data:
-    print(data)
-    n1 = data[1]
-    n2 = data[2]
-    numbers = []
-    numbers.append(n1)
-    numbers.append(n2)
     print(n1, n2)
     numbers = [int(x)for x in numbers]
     print(numbers[0] / numbers[1])
