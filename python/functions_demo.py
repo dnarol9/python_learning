cd895a6a59b54de678d8806bb554c7677583ee2c
dnarol9 <lorand.szabo9@gmail.com>
2022-01-25 10:27:50 +0100

    as

--- python/functions_demo.py
+++ python/functions_demo.py
@@ -53,8 +53,14 @@ print("="*50)
 def f(a, data=[]):
     data.append(a)
     return data
-c = input("start ")
+c = input("start?[y/n] ")
 while c == "y":
     i = int(input("Enter number to append: "))
     print(f(i))
-    c = input("continue?" )
+    c = input("continue?[y/n] ")
+    if c == "n":
+        print(f(i))
+        break
+print("="*50)
+print("Keyword Arguments")
+print("="*50)
