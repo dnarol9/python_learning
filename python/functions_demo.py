89eb07ddda4340a3943f3ca1dcbd3bc4a02c72fd
dnarol9 <lorand.szabo9@gmail.com>
2022-01-25 11:28:56 +0100

    functions part 2

--- python/functions_demo.py
+++ python/functions_demo.py
@@ -62,5 +62,40 @@ while c == "y":
         print(f(i))
         break
 print("="*50)
-print("Keyword Arguments")
+print("a²+b²=c²")
 print("="*50)
+import math
+def c(a,b):
+    return math.sqrt(a*a + b+b)
+if __name__ == '__main__':
+    print(c(3, 5))
+print("="*50)
+print("Higher-order function")
+print("="*50)
+b = int(input("Enter Value: "))
+def givemefive():
+    def add(x):
+        return x + b
+    return add
+add = givemefive()
+i = int(input("Enter Value: "))
+print(add(i))
+print("="*50)
+print("map:")
+print("="*50)
+lst = [1, 2, 3, 4, 5]
+def square(num):
+    return num * num
+print(list(map(square, lst)))
+print("="*50)
+print("args, kwargs:")
+print("="*50)
+def unknown(*args, **kwargs):
+    print(f"We received {len(args)} positional arguments. And they are:")
+    for arg in args:
+        print(arg, end= " ")
+    print("")
+    print(f"We received {len(kwargs)} keyword arguments. And they are:")
+    for k, v in kwargs.items():
+        print(f"key={k} and value={v}")
+print(unknown(30, 90, "kushal", lang="python", editor="vim"))
